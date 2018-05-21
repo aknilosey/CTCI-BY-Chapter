c2bf1b6e3c8d07f735ac75c76d310cf57d12eaaf
aknilosey <nilosey.akanksha@gmail.com>
2018-05-21 15:00:53 -0400

    extra parameter to TreeNode

--- Chapter-4/TreeNode.java
+++ Chapter-4/TreeNode.java
@@ -2,9 +2,11 @@ public class TreeNode {
     int value;
     TreeNode left;
     TreeNode right;
+    TreeNode parent;
     public TreeNode(int value) {
         this.value = value;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 }
